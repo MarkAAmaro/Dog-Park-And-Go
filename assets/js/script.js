0cdd6578f33ab86737baee4b6a724ac9a42089fe
Kristian Ramirez <Kristian@sids-MacBook-Pro-2.local>
2023-05-29 18:07:35 -0500

    I made a lot of changes. I will detail them out in my pull request but at this point the user can input a zip code and it will pull up the current weather. Dog parks within a 20 mile radius load in the console log we will need to create html and then write a function to display them on the page. The map still needs to tie into the search input.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 const apiKey = '599e78c3459a8c73ce2b3faeb84e514d';
 
 
+//save zip code to local storage
+document.querySelector('main').addEventListener('click', function (event) {
+  // Check if the event target is the go-button
+  if (event.target.matches('#go-button')) {
+    saveZipCode(event);
+  }
+});
+
 function saveZipCode(event) {
   event.preventDefault();
   var zipCodeInput = document.getElementById('zipCode');
@@ -8,32 +16,32 @@ function saveZipCode(event) {
 
   localStorage.setItem('zipCode', zipCode);
 
-  //Redirect to indexhtml page
-  window.location.href = 'index.html';
+  //Redirect 
+  window.location.href = 'result.html';
 }
 
-document.getElementById('weather-btn').addEventListener('click', function() {
-    const cityInput = document.getElementById('city-input').value;
-    searchWeather(cityInput);
-  });
+//fetch weather based on zipcode from local storage
+function zipCodefromLocal() {
+  var zipCode = localStorage.getItem('zipCode');
+  if (zipCode) {
+    searchWeather(zipCode);
+    searchDogParks(zipCode);
+  }
 
-document.getElementById('search-btn').addEventListener('click', function() {
-  const cityInput = document.getElementById('city-input').value;
-  searchWeather(cityInput);
-});
-
-
-function searchWeather(city) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+}
+//pull weather based on zipcode
+//copied Mark's code but will need to change code in html
+function searchWeather(zipCode) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}`)
     .then(response => response.json())
     .then(data => {
       displayCurrentWeather(data);
+      console.log(data);
     })
     .catch(error => {
       console.log('Error:', error);
     });
 }
-
 function displayCurrentWeather(data) {
   const city = data.name;
   const temperature = data.main.temp;
@@ -48,19 +56,67 @@ function displayCurrentWeather(data) {
   document.getElementById('weather-icon').innerHTML = `<img src="http://openweathermap.org/img/w/${icon}.png" alt="Weather Icon">`;
 }
 
-function initMap() {
-    var options = {
-        zoom:8,
-        center:{lat:29.4252,lng:-98.4946}
+window.addEventListener('load', zipCodefromLocal);
+
+//load dog parks based on zipcode
+function searchDogParks(zipCode) {
+  var geocoder = new google.maps.Geocoder();
+  var address = zipCode;
+
+  geocoder.geocode({ address }, function (results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      var location = results[0].geometry.location;
+      var lat = location.lat();
+      var lng = location.lng();
+
+      //places API
+      var service = new google.maps.places.PlacesService(document.createElement('div'));
+      var request = {
+        location: new google.maps.LatLng(lat, lng),
+        radius: '32186.9',  //within 20 miles can changes later
+        type: 'park',
+        keyword: 'dog park',
+      };
+
+      service.nearbySearch(request, function (results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          results.forEach(function (place) {
+            console.log(place.name);
+            console.log(place.vicinity);
+          });
+        } else {
+          console.log('Error: ', status);
+        }
+      });
+    } else {
+      console.log('Geocode was not successful for the following reason', status);
     }
+  });
+}
 
-    var map = new google.maps.Map(document.getElementById("map"), options);
+function initMap() {
+  var options = {
+   zoom: 8,
+    center: { lat: 29.4252, lng: -98.4946 }
+  }
 
-    var marker = new google.maps.Marker({
-        position:{lat:29.4260,lng:-98.4861},map:map
-    //    icon: image
-    });
+  var map = new google.maps.Map(document.getElementById("map"), options);
+
+  var marker = new google.maps.Marker({
+    position: { lat: 29.4260, lng: -98.4861 }, map: map, 
+    //icon: image
+  });
 }
 
 
 initMap();
+
+
+//previous code 
+//I don't see a weather-btn in the html
+//document.getElementById('weather-btn').addEventListener('click', function () {
+//const cityInput = document.getElementById('city-input').value;
+// searchWeather(cityInput);
+//});
+
+
